94624c6dd09976f5b9dde7a6d61e7b9951870d2c
KietlinskiA <adrian.kietlinski@interia.pl>
2022-08-01 19:00:02 +0200

    Initial commit

---
+++ src/main/java/pl/kietlinski/teaiexercises/TeaiExercisesApplication.java
@@ -0,0 +1,13 @@
+package pl.kietlinski.teaiexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaiExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeaiExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kietlinski/teaiexercises/TeaiExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kietlinski.teaiexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaiExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
